6c7fb57873d31a068547fa22006a1789a1498bc0
Ola Czyrnek <aleksandra@czyrnek.net>
2024-03-14 16:21:08 +0100

    feat(navbar): Install react scroll

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link
+            activeClass="active"
+            to="section1"
+            spy={true}
+            smooth={true}
+            offset={-70} // Offset może być dostosowany do Twojego layoutu
+            duration={500}
+          >
+            Section 1
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="section2"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Section 2
+          </Link>
+        </li>
+        {/* Dodaj kolejne linki dla innych sekcji */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
